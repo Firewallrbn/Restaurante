3fdf97120c8759e7bf3e3815ed786722ed19f158
Julianagza <Julianagza@unisabana.edu.co>
2025-05-03 18:22:43 -0500

    Login (solo esta el archivo no sirve)

--- Restaurante/menu/urls.py
+++ Restaurante/menu/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from menu import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('menu', views.index),
     path('contacto', views.contacto),
+    path('login/', auth_views.LoginView.as_view()),
 ]
