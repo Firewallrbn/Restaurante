5fb15d8d9567f5a86509c9cebe4f312b8847f80d
Julianagza <Julianagza@unisabana.edu.co>
2025-05-03 12:31:44 -0500

    Modelos errores y pensando en los fixtures (help)

--- Restaurante/menu/admin.py
+++ Restaurante/menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Información adicional", {'fields': ('direccion', 'numero')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- Restaurante/menu/models.py
+++ Restaurante/menu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser
+from django.conf import settings
 
-# Create your models here.
+class Platillo(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    ruta_imagen = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    platillos = models.ManyToManyField(Platillo)
+    precio_total = models.DecimalField(max_digits=8, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pedido #{self.id} de {self.cliente.username}"
+
+class CustomUser(AbstractUser):
+    direccion = models.CharField(max_length=255, blank=True, null=True)
+    numero = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
