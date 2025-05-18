ad63ca9d54909421d7923da07d6dddfe447f995f
Juan David Cruz Angel <161855153+Firewallrbn@users.noreply.github.com>
2025-05-17 19:35:57 -0500

    1-Prueba pedidos staff

--- menu/urls.py
+++ menu/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('contacto/', views.contacto),
     path('login/', auth_views.LoginView.as_view()),
     path('prueba/', views.MostrarPlatillos),
-    path('api/menu', views.api_menu)
+    path('api/menu', views.api_menu),
+    path('api/pedido/', views.crear_pedido, name='crear_pedido'),
+    path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
+    path('pedidos/entregar/<int:pedido_id>/', views.marcar_entregado, name='marcar_entregado'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Platillo, Pedido
+from .models import Platillo, Pedido,CustomUser
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required  
+from django.shortcuts import redirect                       
 
 def index(request):
     return render(request, 'index.html') 
@@ -28,3 +32,42 @@ def api_menu(request):
         })
 
     return JsonResponse({"data": resultado})
+
+@csrf_exempt
+def crear_pedido(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        
+        nombre = data["nombreCliente"]
+        telefono = data["telefonoCliente"]
+        direccion = data["direccionCliente"]
+        productos = data["listaProductos"]
+        total = data["valorTotal"]
+
+        # Buscar o crear el cliente (puedes hacer login real más adelante)
+        cliente, _ = CustomUser.objects.get_or_create(username=telefono, defaults={
+            "first_name": nombre,
+            "numero": telefono,
+            "direccion": direccion,
+            "password": "cliente"  # por defecto, luego puedes manejar login real
+        })
+
+        pedido = Pedido.objects.create(cliente=cliente, precio_total=total)
+        
+        for item in productos:
+            platillo = Platillo.objects.get(nombre=item["nombre"])
+            pedido.platillos.add(platillo)
+
+        return JsonResponse({"mensaje": "Pedido creado con éxito", "id": pedido.id})
+    
+@login_required
+def lista_pedidos(request):
+    pedidos = Pedido.objects.select_related('cliente').all().order_by('-fecha')
+    return render(request, 'menu/pedidos.html', {'pedidos': pedidos})
+
+@login_required
+def marcar_entregado(request, pedido_id):
+    pedido = Pedido.objects.get(id=pedido_id)
+    pedido.Entregado = True
+    pedido.save()
+    return redirect('lista_pedidos')
