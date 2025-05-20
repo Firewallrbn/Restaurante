9976f7c1838dcb1688c3a9c465a9a6f199bd11e0
Julianagza <Julianagza@unisabana.edu.co>
2025-05-20 15:09:28 -0500

    otro cambio de linea

--- menu/views.py
+++ menu/views.py
@@ -6,7 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required  
 from django.shortcuts import redirect  
 from django.db.models import Q                       
-from rest_framework.views import APIView, permission_classes
+from rest_framework.views import APIView
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response  
 def index(request):
