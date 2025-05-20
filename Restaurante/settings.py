e91ec095d7a24ac3d49f3f4541870f64f7fbb550
Julianagza <Julianagza@unisabana.edu.co>
2025-05-20 13:31:26 -0500

    Revert "Primer intento de tokens"
    This reverts commit dc753f3ee021a2d442b5871f9d24871f05a696d7.

--- Restaurante/settings.py
+++ Restaurante/settings.py
@@ -101,11 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
--- menu/views.py
+++ menu/views.py
@@ -5,14 +5,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required  
 from django.shortcuts import redirect  
-from django.db.models import Q         
-from rest_framework_authentication import token_authentication
-from rest_framework.permissions import IsAuthenticated
-form rest_framework.decorators import authentication_classes, permission_classes         
-from django.shortcuts import redirect
-
-def Redirect(request):
-    return redirect('/menu/')  
+from django.db.models import Q                       
 
 def index(request):
     return render(request, 'index.html') 
@@ -25,8 +18,7 @@ def MostrarPlatillos(request): #Solo para pruebas
     resultado = "\n".join([f"{P.nombre} - {P.descripcion}" for P in platillos])
     return HttpResponse(f"<pre>{resultado}</pre>")
 
-@authentication_classes([token_authentication])
-@permission_classes([IsAuthenticated])
+
 def api_menu(request):
     datos = Platillo.objects.all()
     resultado = []
@@ -70,8 +62,6 @@ def crear_pedido(request):
         return JsonResponse({"mensaje": "Pedido creado con éxito", "id": pedido.id})
     
 @login_required
-@authentication_classes([token_authentication])
-@permission_classes([IsAuthenticated])
 def lista_pedidos(request):
     # 1. Traemos TODOS los pedidos
     pedidos = Pedido.objects.select_related('cliente').order_by('-fecha')
@@ -93,8 +83,6 @@ def lista_pedidos(request):
 
 
 @login_required
-@authentication_classes([token_authentication])
-@permission_classes([IsAuthenticated])
 def marcar_entregado(request, pedido_id):
     pedido = Pedido.objects.get(id=pedido_id)
     pedido.Entregado = True
