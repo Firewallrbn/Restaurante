87e5ec48c750608d1da063c8526414157c076af8
Julianagza <Julianagza@unisabana.edu.co>
2025-05-22 11:11:11 -0500

    .

--- menu/urls.py
+++ menu/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('/', views.index),
-    path('logout/', cerrar_sesion, name='logout'),
+    path('logout/', views.cerrar_sesion, name='logout'),
     path('contacto/', views.contacto),
     path('login/', auth_views.LoginView.as_view()),
     path('prueba/', views.MostrarPlatillos),
